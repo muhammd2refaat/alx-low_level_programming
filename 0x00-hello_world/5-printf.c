217a13ddc817776f12ee961fc7c0a12ef4544606
Your Name <you@example.com>
2023-06-15 19:34:12 -0700

    Done

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,15 +1,11 @@
-#include <stdio.h>
-
+#include<stdio.h>
 /**
- * main - Entry point
- *
- * Description: A C program that points with printf function.
- *
- * Return: Always 0 (Success)
+* main - Entry point
+*
+* Return: Always 0 (Success)
 */
-
 int main(void)
 {
-	printf("with proper grammer. but the outcome is a piece of art.\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
